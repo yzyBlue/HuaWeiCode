b983464b934fa8ce18066801dabf7ef91fad65a8
unknown <Yuan.Ziyang@YuanZiyang-01.vico-lab.com>
2016-03-05 19:29:39 +0800

    提交测试用例文件

---
+++ future_net/src/com/util/test/FileUtilTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.util.test;
+
+
+import org.junit.Test;
+
+import com.filetool.util.FileUtil;
+import com.filetool.util.LogUtil;
+
+/**
+ * @author Yuan.Ziyang
+ *
+ */
+public class FileUtilTest {
+		@Test
+		public void readTest(){
+			 LogUtil.printLog("Begin");
+			 String filePath="resource/graph.csv";
+			String content=FileUtil.read(filePath,null);
+			System.out.println(content);
+			LogUtil.printLog("End");
+		}
+}
